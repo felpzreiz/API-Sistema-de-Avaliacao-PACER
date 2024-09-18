8a3a2ad594efda5d9a9a126f1f59858b5180d61c
José Wesley <163480497+jwesleey@users.noreply.github.com>
2024-09-18 20:02:26 -0300

    ✨ feat: Página de login

---
+++ PacerAlphaCode/src/main/java/org/example/paceralphacode/ExecuteApplication.java
@@ -0,0 +1,23 @@
+package org.example.paceralphacode;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ExecuteApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ExecuteApplication.class.getResource("Tela.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}

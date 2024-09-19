21f0956d6ce163ed4233e5f4090a06b87d71565f
Gustavo Mendes <162648811+GustavoNMendes01@users.noreply.github.com>
2024-09-19 20:17:06 -0300

    Create HomeController.java

---
+++ PacerAlphaCode/src/main/java/org/example/paceralphacode/HomeController.java
@@ -0,0 +1,36 @@
+package org.example.paceralphacode;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class HomeController {
+
+    @FXML
+    private Button botaogerenciaraluno;
+
+    @FXML
+    private Button botaogerenciargrupos;
+
+    @FXML
+    private Button botaogerenciarnotas;
+
+    @FXML
+    private Button botaogerenciarsprints;
+
+    @FXML
+    private Button botaohome;
+
+    @FXML
+    private AnchorPane fundo;
+
+    @FXML
+    private ImageView imagemusuario;
+
+    @FXML
+    private AnchorPane ladodireito;
+
+    @FXML
+    private AnchorPane ladoesquerdo;
+
+}

89a9e851a868c0c246dc8d62ad1b608762a6e0ce
jwesleey <ljweesley@gmail.com>
2024-10-16 22:56:27 -0300

    feat: Correção de código

--- PacerAlphaCode/src/main/java/org/example/paceralphacode/Alunos.java
+++ PacerAlphaCode/src/main/java/org/example/paceralphacode/Alunos.java
@@ -3,21 +3,49 @@ package org.example.paceralphacode;
 import javafx.beans.property.SimpleStringProperty;
 
 public class Alunos {
-    private final SimpleStringProperty name;
 
-    public Alunos(String name) {
-        this.name = new SimpleStringProperty(name);
+    String nome, email, grupo, repo;
+
+    public Alunos(String nome, String email, String grupo, String repo) {
+        this.nome = nome;
+        this.email = email;
+        this.grupo = grupo;
+        this.repo = repo;
     }
 
-    public String getName() {
-        return this.name.get();
+    public String getNome() {
+        return nome;
     }
 
-    public void setName(String name) {
-        this.name.set(name);
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
-    public SimpleStringProperty nameProperty() {
-        return this.name;
+    public String getEmail() {
+        return email;
     }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
+    public String getRepo() {
+        return repo;
+    }
+
+    public void setRepo(String repo) {
+        this.repo = repo;
+    }
+
 }
+
+
+
--- PacerAlphaCode/src/main/java/org/example/paceralphacode/GerenciarAlunoController.java
+++ PacerAlphaCode/src/main/java/org/example/paceralphacode/GerenciarAlunoController.java
@@ -5,10 +5,17 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class GerenciarAlunoController {
 
@@ -37,60 +44,65 @@ public class GerenciarAlunoController {
     @FXML
     private Label nStudents;
 
-    private ObservableList<org.example.paceralphacode.Alunos> Alunos;
+    @FXML
+    private TableColumn<Alunos, String> viewEmail;
+
+    @FXML
+    private TableColumn<Alunos, String> viewGroup;
+
+    @FXML
+    private TableColumn<Alunos, String> viewName;
+
+    @FXML
+    private TableView<Alunos> viewStudent;
+
 
     @FXML
     private AnchorPane gAlunos;
 
-    @FXML
-    private TableColumn<org.example.paceralphacode.Alunos, String> tableViewStudent;
-
-    @FXML
-    private TableView<org.example.paceralphacode.Alunos> viewStudent;
+    private ObservableList<Alunos> listaDados;
+    private Set<String> csvImport;
 
     public void initialize() {
-        Alunos = FXCollections.observableArrayList();
 
-        tableViewStudent.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
-        tableViewStudent.setCellFactory(TextFieldTableCell.forTableColumn());
+        listaDados = FXCollections.observableArrayList();
+        csvImport = new HashSet<>();
 
-        tableViewStudent.setOnEditCommit(event -> {
-            org.example.paceralphacode.Alunos aluno = event.getRowValue();
-            aluno.setName(event.getNewValue());
-        });
+        viewName.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        viewEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        viewGroup.setCellValueFactory(new PropertyValueFactory<>("grupo"));
 
-        buttonAddStudent.setOnAction(event -> buttonAddStudent());
-
-        viewStudent.setItems(Alunos);
         style();
     }
 
+
     @FXML
     private void buttonAddStudent() {
-        String Aluno = writeStudent.getText();
+        String email = writeStudent.getText();
         if (writeStudent.getText().isEmpty()) {
             checkStudent.setText("Informe o e-mail do aluno.");
             checkStudent.setVisible(true);
         } else {
             checkStudent.setVisible(false);
         }
-        if (Aluno != null && !Aluno.isEmpty()) {
-            Alunos.add(new Alunos(Aluno));
+        if (email != null && !email.isEmpty()) {
+            Alunos novoAluno = new Alunos("", email, "", "");
+            listaDados.add(novoAluno);
             writeStudent.clear();
             nStudents();
         }
     }
 
     private void nStudents() {
-        int qtd = Alunos.size();
+        int qtd = listaDados.size();
         nStudents.setText(String.valueOf(qtd));
     }
 
     @FXML
     private void removeSelectedStudent() {
-        org.example.paceralphacode.Alunos selectedStudent = viewStudent.getSelectionModel().getSelectedItem();
+        Alunos selectedStudent = viewStudent.getSelectionModel().getSelectedItem();
         if (selectedStudent != null) {
-            Alunos.remove(selectedStudent);
+            listaDados.remove(selectedStudent);
             checkStudent1.setVisible(false);
             nStudents();
         } else {
@@ -108,13 +120,46 @@ public class GerenciarAlunoController {
         buttonEditStudent.getStylesheets().add(css);
         buttonBuscarStudent.getStylesheets().add(css);
 
-
     }
 
     public void EditedSelectedStudent(ActionEvent actionEvent) {
     }
 
-    public void ImportSelectedStudent(ActionEvent actionEvent) {
+    public void ImportSelectedStudent() {
+        FileChooser search = new FileChooser();
+        search.setTitle("Selecione o arquivo CSV");
+        search.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV", "*.csv"));
+
+        File arquivoCSV = search.showOpenDialog(new Stage());
+        if (arquivoCSV != null) {
+            importCSV(arquivoCSV);
+        }
+
+    }
+
+    public void importCSV(File arquivo) {
+        String linha;
+        try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {                 // BufferedReader Lê o arquivo linha por linha - FileReader é o leitor do arquivo
+            br.readLine();                                                                                                                 // br.read.Line é utilizado para ler linha por linha
+            while ((linha = br.readLine()) != null) {                                                                       // Enquanto a linha for diferente de nula vai continuar lendo
+                String[] texto = linha.split(";");                                                                  // Criado um Array com o nome texto ela recebe o texto delimitado pelo ";" linha.split é uma função da classe String para dividir a string
+
+                String nome = (texto.length > 1) ? texto[0].trim() : "";
+                String email = texto[1].trim();
+                String grupo = (texto.length > 1) ? texto[2].trim() : "";                                                                                          //Considerando que a posição 0 seja o nome .trim() ignora espaços vazios
+                String repo = (texto.length > 1) ? texto[3].trim() : "";                                                                                               //  grupo recebe o a posição 1, porém para ignorar o vazio foi feito um operador ternario (condição) ? valor_se_verdadeiro : valor_se_falso
+                // O arquivo dá erro caso encontre uma coluna vazia devido o array, deste modo considerei vazio
+                if (!csvImport.contains(email)) {                                                                                // Por meio do HashSet eu verifico as duplicatas de acordo com os email que eu já adicionei
+                    Alunos aluno = new Alunos(nome, email, grupo, repo);                                                                         // Instanciado um novo objeto aluno para receber os atributos
+                    listaDados.add(aluno);                                                                                                    // adiciona os valores na lista observável
+                    csvImport.add(email);                                                                                                    // Guarda o email repetido para uma lista
+                }
+
+            }
+            viewStudent.setItems(listaDados);                                                                                              // Envia os valores para a tabela
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void buttonBuscarStudent(ActionEvent actionEvent) {

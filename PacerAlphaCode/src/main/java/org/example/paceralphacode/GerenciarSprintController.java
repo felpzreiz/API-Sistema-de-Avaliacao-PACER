429ba4e2eaaad71f8e233522aec1f35820514e6f
Joaovsnas <166765258+Joaovsnas@users.noreply.github.com>
2024-10-19 15:58:55 -0300

    Add files via upload

--- PacerAlphaCode/src/main/java/org/example/paceralphacode/GerenciarSprintController.java
+++ PacerAlphaCode/src/main/java/org/example/paceralphacode/GerenciarSprintController.java
@@ -1,15 +1,74 @@
-package org.example.paceralphacode;
-
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
-public class GerenciarSprintController {
+public class PleaseProvideControllerClassName {
 
     @FXML
-    private ImageView fatec;
+    private TextField addCriterio;
 
     @FXML
-    private AnchorPane gSprints;
+    private DatePicker addData;
+
+    @FXML
+    private Button btnAdicionarC;
+
+    @FXML
+    private Button btnAdicionarS;
+
+    @FXML
+    private Button btnEditar;
+
+    @FXML
+    private Button btnRemoverC;
+
+    @FXML
+    private Button btnRemoverS;
+
+    @FXML
+    private TableColumn<?, ?> clnData;
+
+    @FXML
+    private TableColumn<?, ?> clnSprint;
+
+    @FXML
+    private TableColumn<?, ?> tblCriterio;
+
+    @FXML
+    private TableView<?> tblSprint;
+
+    @FXML
+    void Adicionarcriterio(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Removercriterio(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Removersprint(ActionEvent event) {
+
+    }
+
+    @FXML
+    void acaocriterio(ActionEvent event) {
+
+    }
+
+    @FXML
+    void acaodata(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editarsprint(ActionEvent event) {
+
+    }
 
 }

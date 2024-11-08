cfb4eb0603697040f197ac4206e6163eb0e04bfc
yzvictorr <165969046+yzvictorr@users.noreply.github.com>
2024-11-08 20:01:11 -0300

    feat: RedefinirSenha

---
+++ PacerAlphaCode/src/main/java/org/alphacode/pacer/test/RedefinirSenha.java
@@ -0,0 +1,41 @@
+package org.alphacode.pacer.test;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+public class RedefinirSenha {
+
+    @FXML
+    private ImageView Alpha;
+
+    @FXML
+    private AnchorPane PaneSenha;
+
+    @FXML
+    private Label alSenha;
+
+    @FXML
+    private Button btSalvar;
+
+    @FXML
+    private PasswordField confirmarSenha;
+
+    @FXML
+    private Label crSenha;
+
+    @FXML
+    private GridPane gridSenha;
+
+    @FXML
+    private PasswordField nvSenha;
+
+    @FXML
+    private TextField snAtual;
+
+}

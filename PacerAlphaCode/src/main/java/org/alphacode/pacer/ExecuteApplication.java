ef87444acfdb7cc81d68f927a6d9951116f3ccb8
jwesleey <ljweesley@gmail.com>
2024-11-12 22:15:04 -0300

    feat: Ajustes Main e Executável

--- PacerAlphaCode/src/main/java/org/alphacode/pacer/ExecuteApplication.java
+++ PacerAlphaCode/src/main/java/org/alphacode/pacer/ExecuteApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class ExecuteApplication extends Application {
@@ -13,10 +14,11 @@ public class ExecuteApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Login");
         stage.setScene(scene);
-       // stage.setResizable(false);
+        stage.setResizable(false);
         stage.show();
 
     }
+
     public static void main(String[] args) {
         launch();
     }
